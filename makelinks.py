427a9a51f5340669819239a45da0784cb772181f
BruceYan143 <150405522+BruceYan143@users.noreply.github.com>
2025-02-05 15:05:50 -0500

    Update makelinks.py

--- makelinks.py
+++ makelinks.py
@@ -4,6 +4,7 @@ import re
 import os
 #Andrew was here
 #Graham here
+#Bruce here
 # https://docs.google.com/forms/d/e/1FAIpQLSe42v3lgS8YUPTlYIkupuH56MhJzeaRakqZTjE9lRhuP1U5ow/viewform?usp=pp_url&entry.1290452009=Python+Club
 
 
